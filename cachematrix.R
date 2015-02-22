d159b001968645f3a35b047ca8f5859865b7a3e0
Daniel <danielpriver@yahoo.com>
2015-02-21 23:10:11 -0500

    committing files

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+## makeCacheMatrix Creates a list of functions to be passed 
+##to the  Cache solve function
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  
+  #initializes the variable m
+  m <- NULL
+  
+  ## uses the << to create a global variable y and
+  ## set any previous global variable m to NUll
+  
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  
+  
+  get <- function() x
+  setinv <- function(inv) m <<- inv
+  getinv <- function() m
+  
+  ## the output a list of 4 functions:set,get,setinv,getinv
+  
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## cacheSolve retrieves the inverse matrix from the global
+## environment. If no matrix is found it inverts the matrix
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  
+  ## retrieve the inverse matrix m and display message
+  m <- x$getinv()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  ## if can't find the inverse matrix then solve the matrix
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinv(m)
+  m
 }
